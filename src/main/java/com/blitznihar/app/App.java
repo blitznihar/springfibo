ee69b8505ad8ce50bb6c258058eabba535d8f090
blitznihar <blitznihar@gmail.com>
2019-09-02 16:16:50 -0500

    Removing Spring Boot

--- src/main/java/com/blitznihar/app/App.java
+++ src/main/java/com/blitznihar/app/App.java
@@ -1,32 +1,35 @@
 package com.blitznihar.app;
 
+
 import com.blitznihar.fibo.Fibonaci;
 import com.blitznihar.fibo.IFibonaci;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+// import org.slf4j.Logger;
+// import org.slf4j.LoggerFactory;
+// import org.springframework.boot.SpringApplication;
+// import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
-
+import org.springframework.context.annotation.Configuration;
 /**
  * Hello world!
  *
  */
-@SpringBootApplication
+// @SpringBootApplication
+@Configuration
 @ComponentScan(basePackages = {"com.blitznihar.app", "com.blitznihar.fibo"})
 public class App 
 {
-    private static Logger LOGGER = LoggerFactory.getLogger(App.class);
+    // private static Logger LOGGER = LoggerFactory.getLogger(App.class);
     public static void main( String[] args )
     {
-        ApplicationContext ctx =  SpringApplication.run(App.class, args);
+        ApplicationContext ctx =  new AnnotationConfigApplicationContext(App.class);
         //Fibonaci.print(new Fibonaci().getFiboUntil(10));
         IFibonaci fibonaci = (Fibonaci)ctx.getBean(Fibonaci.class);
         
         Fibonaci.print(fibonaci.getFiboUntil(10));
-        LOGGER.info("{}",fibonaci);
+        // LOGGER.info("{}",fibonaci);
 
 
     }
